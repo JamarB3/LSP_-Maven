c9f9c286859926083db296773995a5f73b1fa46f
JamarB3 <102191390+JamarB3@users.noreply.github.com>
2023-02-06 17:28:38 -0500

    Attempting to solve "unable to access resources" error

--- lspSpring2023/src/org/howard/edu/lsp/assignment2/Driver.java
+++ lspSpring2023/src/org/howard/edu/lsp/assignment2/Driver.java
@@ -5,9 +5,9 @@ import java.io.FileNotFoundException;
 public class Driver {
 
 	public static void main(String[] args) {
-		FileReader fr = new FileReader();
+		FileReader file = new FileReader();
 		try {
-			System.out.println(fr.readToString("main/java/resources/words.txt"));
+			System.out.println(file.readToString("main/java/resources/words.txt"));
 		} catch (FileNotFoundException e) {
 			System.out.println("Unable to open resource");
 		}
