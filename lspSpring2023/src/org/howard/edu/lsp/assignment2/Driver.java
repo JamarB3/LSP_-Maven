47b56c3977a617c18b5d875210a1cae42b53446e
JamarB3 <102191390+JamarB3@users.noreply.github.com>
2023-02-06 17:18:07 -0500

    Created separate package to house resources for java

--- lspSpring2023/src/org/howard/edu/lsp/assignment2/Driver.java
+++ lspSpring2023/src/org/howard/edu/lsp/assignment2/Driver.java
@@ -7,7 +7,7 @@ public class Driver {
 	public static void main(String[] args) {
 		FileReader fr = new FileReader();
 		try {
-			System.out.println(fr.readToString("src/org.howard.edu.lsp.assignment2/words.txt"));
+			System.out.println(fr.readToString("main/java/resources/words.txt"));
 		} catch (FileNotFoundException e) {
 			System.out.println("Unable to open resource");
 		}
