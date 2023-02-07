7757cd847ca4e6ace8eb2e1183f1b7f7738a8bc4
JamarB3 <102191390+JamarB3@users.noreply.github.com>
2023-02-06 23:04:40 -0500

    Driver reflects restored word.txt file by having line 14 changed back to resources, then words.txt

--- lspSpring2023/src/org/howard/edu/lsp/assignment2/Driver.java
+++ lspSpring2023/src/org/howard/edu/lsp/assignment2/Driver.java
@@ -11,7 +11,7 @@ public class Driver {
 	public static void main(String[] args) {
 		FileReader fr = new FileReader();
 		try {
-			fr.readString("main/java/words.txt");
+			fr.readString("main/java/resources/words.txt");
 			//System.out.println(fr.readString("main/java/words.txt"));
 		} catch (FileNotFoundException e) {
 			System.out.println("Unable to open resource");
