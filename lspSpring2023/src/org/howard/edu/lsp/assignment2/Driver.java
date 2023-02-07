360c84b1fa39d00aab6369579805c7a4efb7b5cb
JamarB3 <102191390+JamarB3@users.noreply.github.com>
2023-02-06 22:55:21 -0500

    One big commit which features minor change (commenting out System.out) on Driver and added functionality to FileReader, via hashtables.

--- lspSpring2023/src/org/howard/edu/lsp/assignment2/Driver.java
+++ lspSpring2023/src/org/howard/edu/lsp/assignment2/Driver.java
@@ -3,11 +3,11 @@ package org.howard.edu.lsp.assignment2;
 import java.io.FileNotFoundException;
 
 public class Driver {
-
 	public static void main(String[] args) {
-		FileReader file = new FileReader();
+		FileReader fr = new FileReader();
 		try {
-			System.out.println(file.readToString("main/java/resources/words.txt"));
+			fr.readString("main/java/words.txt");
+			//System.out.println(fr.readString("main/java/words.txt"));
 		} catch (FileNotFoundException e) {
 			System.out.println("Unable to open resource");
 		}
--- lspSpring2023/src/org/howard/edu/lsp/assignment2/FileReader.java
+++ lspSpring2023/src/org/howard/edu/lsp/assignment2/FileReader.java
@@ -1,3 +1,8 @@
+/**
+ * Name: Jamar Bailey III
+ */
+
+
 package org.howard.edu.lsp.assignment2;
 
 import java.io.File;
@@ -5,24 +10,33 @@ import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.Scanner;
 
+import java.util. *; 
 public class FileReader {
-	public String readToString(String resource) throws FileNotFoundException {
+	public String readString(String resource) throws FileNotFoundException {
 		URL url = getClass().getClassLoader().getResource(resource);
-		
+		Hashtable<String, Integer> word_counter = new Hashtable<String, Integer>(); // Creates new Hashtable, which will hold values
 		if (url != null) {
 			File file = new File(url.getPath());
 			
-			Scanner sc = null;
-			try {
-				sc = new Scanner(file);
-				String output = "";
-				while (sc.hasNextLine()) {
-					output = output + sc.nextLine() + " ";
+			Scanner MyObj = null;
+			MyObj = new Scanner(file);
+			String word = "";
+			while (MyObj.hasNext()) { //Gets each next word accordingly
+				word = MyObj.next();
+				if (word.length() > 3 && !word.matches("^[0-9]+$") && !word.matches("^[^a-zA-Z0-9]+$")) {
+					word = word.toLowerCase(); //Above line filters out words with 3 letters or less, special characters, and integers
+					if (word_counter.containsKey(word));
+						word_counter.put(word, word_counter.getOrDefault(word, 0) + 1); //Places word, in lower case, into hash table.
+					    //If it is already in the hash table, one is added to the value.
+				} else {
+					continue;
 				}
-				return output.trim();
-			} finally {
-				sc.close();
 			}
+			MyObj.close();
+			// Code below will print out the word as well as their associated count
+			for (Map.Entry<String,Integer> entry: word_counter.entrySet())
+				System.out.println(entry.getKey() + " " + entry.getValue());
+			
 		}
 		throw new FileNotFoundException();
 	}
