8b28d3c6b13ae922a0aa42ead8e68ba7ef71e816
JamarB3 <102191390+JamarB3@users.noreply.github.com>
2023-03-14 22:16:39 -0400

    Reaffirming push properly worked

--- lspSpring2023/src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
+++ lspSpring2023/src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
@@ -1,5 +1,17 @@
 package org.howard.edu.lsp.assignment5;
 
+import org.junit.*;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+
+
 public class IntegerSetTest {
+	private List<Integer> new_set = new ArrayList<Integer>();
+	@Test
+	@DisplayName("Test case for clear")
+	public void testclear() {
+		
+	}
 
 }
