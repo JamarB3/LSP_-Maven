4cebef96ace8b85ed56de3d014a61e3de9420ca1
JamarB3 <102191390+JamarB3@users.noreply.github.com>
2023-03-15 23:13:35 -0400

    Added more test cases to methods

--- lspSpring2023/src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
+++ lspSpring2023/src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
@@ -21,6 +21,11 @@ public class IntegerSetTest {
     @Test
     @DisplayName("Test case for clear")
     public void testClear() {
+    	
+    	//Tests for clearing an already empty set
+    	integerSet.clear();
+        Assertions.assertTrue(integerSet.isEmpty());
+    	
         integerSet.add(1);
         integerSet.add(2);
         integerSet.clear();
@@ -30,28 +35,66 @@ public class IntegerSetTest {
     @Test
     @DisplayName("Test case for length")
     public void testLength() {
+    	
+    	
+    	//Tests for empty set, should be 0
+    	Assertions.assertEquals(0, integerSet.length());
+    	
+    	
         integerSet.add(1);
         integerSet.add(2);
         Assertions.assertEquals(2, integerSet.length());
+        
+        //Test case for a very large set
+        for (int num = 1; num <= 100; num++) {
+            integerSet.add(num);
+        }
+        Assertions.assertEquals(100, integerSet.length());
     }
 
     @Test
     @DisplayName("Test case for equal")
     public void testEquals() {
+    	
+    	
         IntegerSet b = new IntegerSet();
+        //Tests empty set
+        Assertions.assertTrue(integerSet.equals(b));
+        
         b.add(2);
         b.add(1);
         integerSet.add(1);
+        
+        //Tests different size set
+        Assertions.assertFalse(integerSet.equals(b));
+        
+        //Normal test, both should be equal by this point
         integerSet.add(2);
         Assertions.assertTrue(integerSet.equals(b));
+        
+        
+        
+        
     }
 
     @Test
     @DisplayName("Test case for contains")
     public void testContains() {
+    	
+    	//Testing empty set contain
+    	Assertions.assertFalse(integerSet.contains(1));
+    	
         integerSet.add(1);
         Assertions.assertTrue(integerSet.contains(1));
         Assertions.assertFalse(integerSet.contains(2));
+        
+        //Tests multiple elements, some in and some not in the set
+        integerSet.add(2);
+        integerSet.add(3);
+        Assertions.assertTrue(integerSet.contains(1));
+        Assertions.assertTrue(integerSet.contains(2));
+        Assertions.assertTrue(integerSet.contains(3));
+        Assertions.assertFalse(integerSet.contains(4));
     }
 
     @Test
@@ -61,9 +104,18 @@ public class IntegerSetTest {
         integerSet.add(2);
         Assertions.assertEquals(2, integerSet.largest());
         
-        
+        //Tests exception
         IntegerSet emptySet = new IntegerSet();
         assertThrows(IntegerSetException.class, () -> emptySet.largest());
+        
+        
+        //Test for duplicates
+        integerSet.add(5);
+        integerSet.add(5);
+        Assertions.assertEquals(5, integerSet.largest());
+       
+        
+        
     }
 
     @Test
@@ -76,6 +128,15 @@ public class IntegerSetTest {
         //Created emptySet to test for empty set edge case
         IntegerSet emptySet = new IntegerSet();
         assertThrows(IntegerSetException.class, () -> emptySet.smallest());
+        
+       
+    
+        
+        //Test case for smallest with negative elements
+        integerSet.add(-3);
+        integerSet.add(-1);
+        integerSet.add(-2);
+        Assertions.assertEquals(-3, integerSet.smallest());
     }	
 
     @Test
@@ -101,9 +162,14 @@ public class IntegerSetTest {
     @Test
     @DisplayName("Test case for remove")
     public void testRemove() throws IntegerSetException {
+    	
+    	
         integerSet.add(1);
         integerSet.remove(1);
         Assertions.assertTrue(integerSet.isEmpty());
+        
+        
+        
     }
         
         
@@ -111,14 +177,26 @@ public class IntegerSetTest {
     @Test
     @DisplayName("Test case for union")
     public void testUnion() {
+    	
+    	//Tests if empty
+    	IntegerSet intSetc = new IntegerSet();
+        integerSet.union(intSetc);
+        Assertions.assertTrue(integerSet.isEmpty());
+        
+        
+        
         integerSet.add(1);
         integerSet.add(2);
-        IntegerSet intSetb = new IntegerSet();
-        intSetb.add(3);
-        intSetb.add(4);
-        integerSet.union(intSetb);
+        IntegerSet intSetd = new IntegerSet();
+        intSetd.add(3);
+        intSetd.add(4);
+        integerSet.union(intSetd);
         Assertions.assertEquals(4, integerSet.length());
+        
+
+       
     }
+    
 
     @Test
     @DisplayName("Test case for intersect")
@@ -130,6 +208,16 @@ public class IntegerSetTest {
         intSetb.add(3);
         integerSet.intersect(intSetb);
         Assertions.assertEquals(1, integerSet.length());
+        
+        //Test case for intersect with no common elements
+        integerSet.add(1);
+        integerSet.add(2);
+        IntegerSet intSetc = new IntegerSet();
+        intSetc.add(3);
+        intSetc.add(4);
+        integerSet.intersect(intSetc);
+        Assertions.assertTrue(integerSet.isEmpty());
+        
     }
 
     @Test
@@ -179,14 +267,42 @@ public class IntegerSetTest {
         Assertions.assertTrue(integerSet.isEmpty());
         integerSet.add(1);
         Assertions.assertFalse(integerSet.isEmpty());
+        
+        //Test case for isEmpty with multiple items in the set
+        integerSet.add(1);
+        integerSet.add(2);
+        Assertions.assertFalse(integerSet.isEmpty());
+        
+        
+        //Test case for for removing from set, checking for empty
+        integerSet.add(1);
+        integerSet.add(2);
+        integerSet.remove(1);
+        integerSet.remove(2);
+        Assertions.assertTrue(integerSet.isEmpty());
     }
 
     @Test
     @DisplayName("Test case for toString")
     public void testToString() {
+    	
+    	//For single element set
+    	integerSet.add(1);
+        Assertions.assertEquals("[1]", integerSet.toString());
+        
+        //For duo element set
         integerSet.add(1);
         integerSet.add(2);
         Assertions.assertEquals("[1, 2]", integerSet.toString());
+        
+        //For duplicates in Set
+        integerSet.add(1);
+        integerSet.add(2);
+        integerSet.add(1);
+        Assertions.assertEquals("[1, 2]", integerSet.toString());
+        
     }
+    
+    
 }
 
