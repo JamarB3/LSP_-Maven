6f97991f7ea1ef79076c2ec8b7110406ef0fa03d
JamarB3 <102191390+JamarB3@users.noreply.github.com>
2023-04-27 19:09:01 -0400

    Setting up JUNIT test cases

--- lspSpring2023/src/org/howard/edu/lsp/Final/problem/SongsDatabaseTest.java
+++ lspSpring2023/src/org/howard/edu/lsp/Final/problem/SongsDatabaseTest.java
@@ -1,11 +1,16 @@
 package org.howard.edu.lsp.Final.problem;
 
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.Before;
 import org.junit.jupiter.api.DisplayName;
 
 //JUnit test cases go here
 public class SongsDatabaseTest {
+	private SongsDatabase db;
 
+    @Before
+    public void setUp() throws Exception {
+        db = new SongsDatabase();
+    }
 }
