c6d774bfe35a7c52e2fde6bdd602ba2427249081
JamarB3 <102191390+JamarB3@users.noreply.github.com>
2023-04-27 19:35:05 -0400

    Created test cases for all 3 methods

--- lspSpring2023/src/org/howard/edu/lsp/Final/problem/SongsDatabaseTest.java
+++ lspSpring2023/src/org/howard/edu/lsp/Final/problem/SongsDatabaseTest.java
@@ -12,24 +12,54 @@ import org.junit.Before;
 public class SongsDatabaseTest {
 	private SongsDatabase db;
 
-    @Before
-    public void setUp() throws Exception {
-        db = new SongsDatabase();
-    }
-    
-    
-    @Test
-    public void testAddSong() {
-        // Test case 1: add song to existing genre
-        db.addSong("Rap", "Savage");
-        db.addSong("Rap", "Gin and Juice");
-        Set<String> rapSongs = db.getSongs("Rap");
-        assertTrue(rapSongs.contains("Savage"));
-        assertTrue(rapSongs.contains("Gin and Juice"));
+	@Before
+	public void setUp() throws Exception {
+	    db = new SongsDatabase();
+	}
 
-        // Test case 2: add song to new genre
-        db.addSong("Pop", "Shallow");
-        Set<String> popSongs = db.getSongs("Pop");
-        assertTrue(popSongs.contains("Shallow"));
-    }
+	@Test
+	public void testAddSong() {
+	    // Test case 1: add song to existing genre
+		SongsDatabase db = new SongsDatabase();
+	    db.addSong("Rap", "Savage");
+	    db.addSong("Rap", "Gin and Juice");
+	    Set<String> rapSongs = db.getSongs("Rap");
+	    assertTrue(rapSongs.contains("Savage"));
+	    assertTrue(rapSongs.contains("Gin and Juice"));
+
+	    // Test case 2: add song to new genre
+	    db.addSong("Pop", "Shallow");
+	    Set<String> popSongs = db.getSongs("Pop");
+	    assertTrue(popSongs.contains("Shallow"));
+	}
+
+	@Test
+	public void testGetSongs() {
+	    // Test case 1: get songs for existing genre
+		SongsDatabase db = new SongsDatabase();
+	    db.addSong("Rock", "Bohemian Rhapsody");
+	    db.addSong("Rock", "Stairway to Heaven");
+	    Set<String> rockSongs = db.getSongs("Rock");
+	    assertEquals(2, rockSongs.size());
+	    assertTrue(rockSongs.contains("Bohemian Rhapsody"));
+	    assertTrue(rockSongs.contains("Stairway to Heaven"));
+
+	    // Test case 2: get songs for non-existent genre
+	    Set<String> jazzSongs = db.getSongs("Jazz");
+	    assertTrue(jazzSongs.isEmpty());
+	}
+
+	@Test
+	public void testGetGenreOfSong() {
+	    // Test case 1: get genre for existing song
+		SongsDatabase db = new SongsDatabase();
+	    db.addSong("Hip Hop", "Lose Yourself");
+	    String genre = db.getGenreOfSong("Lose Yourself");
+	    assertEquals("Hip Hop", genre);
+
+	    // Test case 2: get genre for non-existent song
+	    String nonExistentGenre = db.getGenreOfSong("Hotel California");
+	    assertNull(nonExistentGenre);
+	}
+
 }
