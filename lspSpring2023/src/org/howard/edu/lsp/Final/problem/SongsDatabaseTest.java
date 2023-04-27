ce4865bd22d680a00165170f3cde97b7e5747866
JamarB3 <102191390+JamarB3@users.noreply.github.com>
2023-04-27 19:13:39 -0400

    Implemented test case suggested in problem prompt, corrected imports

--- lspSpring2023/src/org/howard/edu/lsp/Final/problem/SongsDatabaseTest.java
+++ lspSpring2023/src/org/howard/edu/lsp/Final/problem/SongsDatabaseTest.java
@@ -1,9 +1,12 @@
 package org.howard.edu.lsp.Final.problem;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.Before;
+
 import org.junit.jupiter.api.DisplayName;
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import java.util.Set;
+import org.junit.Before;
+
 
 //JUnit test cases go here
 public class SongsDatabaseTest {
@@ -12,5 +15,21 @@ public class SongsDatabaseTest {
     @Before
     public void setUp() throws Exception {
         db = new SongsDatabase();
+    }
+    
+    
+    @Test
+    public void testAddSong() {
+        // Test case 1: add song to existing genre
+        db.addSong("Rap", "Savage");
+        db.addSong("Rap", "Gin and Juice");
+        Set<String> rapSongs = db.getSongs("Rap");
+        assertTrue(rapSongs.contains("Savage"));
+        assertTrue(rapSongs.contains("Gin and Juice"));
+
+        // Test case 2: add song to new genre
+        db.addSong("Pop", "Shallow");
+        Set<String> popSongs = db.getSongs("Pop");
+        assertTrue(popSongs.contains("Shallow"));
     }
 }
