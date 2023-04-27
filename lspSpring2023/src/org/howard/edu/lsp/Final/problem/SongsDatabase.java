587404ca8ffbb8bde5f41bc2e0539f6601f61e4c
JamarB3 <102191390+JamarB3@users.noreply.github.com>
2023-04-27 18:55:46 -0400

    Added code to getSongs with comments, and added comments to addSong

--- lspSpring2023/src/org/howard/edu/lsp/Final/problem/SongsDatabase.java
+++ lspSpring2023/src/org/howard/edu/lsp/Final/problem/SongsDatabase.java
@@ -10,8 +10,10 @@ public class SongsDatabase {
 	/* Key is the genre, HashSet contains associated songs */
 	private Map<String, HashSet<String>> map = new HashMap<String, HashSet<String>>();
 
-		/* Add a song title to a genre */
+	/* Add a song title to a genre */
 	public void addSong(String genre, String songTitle) {
+		// Add the song to the set of songs associated with given genre.
+        // If the genre doesn't exist in the map, new set is created for it
 		if (!map.containsKey(genre)) {
             map.put(genre, new HashSet<String>());
         }
@@ -19,7 +21,12 @@ public class SongsDatabase {
     }
 	/* Return the Set that contains all songs for a genre */
 	public Set<String> getSongs(String genre) {
-			// Code it!!
+		// Return the set of songs associated with given genre
+        // Or return an empty set if the genre doesn't exist in the map
+		if (!map.containsKey(genre)) {
+            return new HashSet<String>();
+        }
+        return new HashSet<String>(map.get(genre));
 	}
 
 	/* Return genre, i.e., jazz, given a song title */
