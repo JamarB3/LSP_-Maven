75bdab0663f7caefe84c0d7f5f1b1bac929a1161
JamarB3 <102191390+JamarB3@users.noreply.github.com>
2023-04-27 18:35:03 -0400

    Retrying, so that github has right info

--- lspSpring2023/src/org/howard/edu/lsp/Final/problem/SongsDatabase.java
+++ lspSpring2023/src/org/howard/edu/lsp/Final/problem/SongsDatabase.java
@@ -1,5 +1,25 @@
 package org.howard.edu.lsp.Final.problem;
 
+
+
 public class SongsDatabase {
+	/* Key is the genre, HashSet contains associated songs */
+	private Map<String, HashSet<String>> map = 
+			new HashMap<String, HashSet<String>>();
+
+		/* Add a song title to a genre */
+	public void addSong(String genre, String songTitle) {
+			//Code it!!
+		}
+
+	/* Return the Set that contains all songs for a genre */
+	public Set<String> getSongs(String genre) {
+			// Code it!!
+	}
+
+	/* Return genre, i.e., jazz, given a song title */
+	public String getGenreOfSong(String songTitle) {
+			// Code it!!
+	}
 
 }
--- lspSpring2023/src/org/howard/edu/lsp/Final/problem/SongsDatabaseTest.java
+++ lspSpring2023/src/org/howard/edu/lsp/Final/problem/SongsDatabaseTest.java
@@ -1,5 +1,11 @@
 package org.howard.edu.lsp.Final.problem;
 
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+//JUnit test cases go here
 public class SongsDatabaseTest {
 
 }
